bb99302d04b664db36176d54372ae13fc10303f3
luxkao <lucasmelohue@gmail.com>
2025-09-15 15:32:27 -0300

    Initial commit

---
+++ backend/src/test/java/com/luxkao/sistemaocorrencias/SistemaOcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxkao.sistemaocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaOcorrenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
